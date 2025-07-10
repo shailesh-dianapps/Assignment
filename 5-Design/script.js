2ce978a9f54b3a9e6f231fda025772f210a88597
shailesh-dianapps <shailesh.mangal@dianapps.com>
2025-07-10 16:31:36 +0530

    Commit

---
+++ 5-Design/script.js
@@ -0,0 +1,24 @@
+let total = 0;
+let wishlist = new Set();
+
+function addToList(price, productCode, buttonElement){
+    if(wishlist.has(productCode)){
+        alert("This product is already in your wishlist.");
+        return;
+    }
+
+    wishlist.add(productCode);
+    total += price;
+    document.getElementById("wishlist-total").innerText = total;
+    buttonElement.innerText = "Remove from list";
+    buttonElement.onclick = () => removeFromList(price, productCode, buttonElement);
+}
+
+function removeFromList(price, productCode, buttonElement) {
+    if(!wishlist.has(productCode)) return;
+    wishlist.delete(productCode);
+    total -= price;
+    document.getElementById("wishlist-total").innerText = total;
+    buttonElement.innerText = "Add to list";
+    buttonElement.onclick = () => addToList(price, productCode, buttonElement);
+}
